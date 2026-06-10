75785b659179d83cf6c9b4223c9b4c87c45bbbaa
ahmedibrahemes-ai <ahmedibrahemes@gmail.com>
2026-06-10 22:24:40 +0000

    80e754dc-a11a-4f6d-b0fd-6475f88d7351

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,10 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     '.space-z.ai',
     '21.0.16.62',
+    '21.0.16.8',
     '127.0.0.1',
     'localhost',
+    '0.0.0.0',
     'preview-chat-5183ffd3-3923-40b9-b46a-77cdd4193b52.space-z.ai',
   ],
 };
